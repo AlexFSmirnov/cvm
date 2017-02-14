5a7791e7aeb1045bbafb2480a2d4e2f77bf35bc4
AlexFSmirnov <alex.f.smirnov@yandex.ru>
2017-02-14 15:06:09 +0300

    Added moving square

---
+++ js/config.js
@@ -0,0 +1,18 @@
+//WORLD PROPERTIES
+GROUND_LEVEL =
+
+//PLAYER PROPERTIES
+P_SPEED = 10;
+P_WIDTH = 32;
+P_HEIGHT = 32;
+
+//CONTROLS
+command_keys = [32];    // ' '
+
+up_keys = [87, 38];     // 'w'
+left_keys = [65, 37];   // 'a'
+down_keys = [83, 40];   // 's'
+right_keys = [68, 39];  // 'd'
+
+up_left_keys = [81];    // 'q'
+up_right_keys = [69];   // 'e' 
--- js/script.js
+++ js/script.js
@@ -1,4 +1,42 @@
-function onload() {
+function drawBackground() {
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillRect(0, 0, W_WIDTH, W_HEIGHT);
+}
+
+function drawPlayer() {
+    ctx.fillStyle = "rgb(0, 0, 0)";
+    ctx.fillRect(player_x, player_y, P_WIDTH, P_HEIGHT);
+}
+
+function main() {
+    if (isPressed(up_keys))
+        player_y = Math.max(player_y - P_SPEED, 0);
+    if (isPressed(left_keys))
+        player_x = Math.max(player_x - P_SPEED, 0);
+    if (isPressed(down_keys))
+        player_y = Math.min(player_y + P_SPEED, W_HEIGHT - P_HEIGHT);
+    if (isPressed(right_keys))
+        player_x = Math.min(player_x + P_SPEED, W_WIDTH - P_WIDTH);
+
+
+    drawBackground();
+    drawPlayer();
+    setTimeout(main, 30);
+}
+
+function isPressed(keys) {
+    var res = false;
+    for (var i = 0; i < keys.length; i++) {
+        res |= keys_pressed[keys[i]];
+    }
+    return res;
+}
+
+$(document).ready(function(){
+    keys_pressed = [];
+    window.onkeydown = function(e) {keys_pressed[e.keyCode]=true;}
+    window.onkeyup = function(e) {keys_pressed[e.keyCode]=false;}
+
     W_WIDTH = document.documentElement.clientWidth;
     W_HEIGHT = document.documentElement.clientHeight;
 
@@ -7,4 +45,10 @@ function onload() {
     canvas.height = W_HEIGHT;
 
     ctx = canvas.getContext('2d');
-};
+
+
+    player_x = Math.round(W_WIDTH / 2);
+    player_y = Math.round(W_HEIGHT / 5 * 4);
+
+    main();
+});
