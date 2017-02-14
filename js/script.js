212e89847481182041aee19eae0c067afb14e1c3
AlexFSmirnov <alex.f.smirnov@yandex.ru>
2017-02-14 15:31:13 +0300

    Added gravity and jumps

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 function drawBackground() {
     ctx.fillStyle = "#FFFFFF";
     ctx.fillRect(0, 0, W_WIDTH, W_HEIGHT);
+
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0, GROUND_LEVEL, W_WIDTH, 1);
 }
 
 function drawPlayer() {
@@ -8,16 +11,20 @@ function drawPlayer() {
     ctx.fillRect(player_x, player_y, P_WIDTH, P_HEIGHT);
 }
 
+function playerFall() {
+    player_y = Math.min(player_y + GRAVITY - cur_jump, GROUND_LEVEL - P_HEIGHT);
+    cur_jump = Math.max(cur_jump - JUMP_FADE_SPEED, 0);
+}
+
 function main() {
-    if (isPressed(up_keys))
-        player_y = Math.max(player_y - P_SPEED, 0);
     if (isPressed(left_keys))
         player_x = Math.max(player_x - P_SPEED, 0);
-    if (isPressed(down_keys))
-        player_y = Math.min(player_y + P_SPEED, W_HEIGHT - P_HEIGHT);
     if (isPressed(right_keys))
         player_x = Math.min(player_x + P_SPEED, W_WIDTH - P_WIDTH);
-
+    if (isPressed(up_keys) && player_y == GROUND_LEVEL - P_HEIGHT) {
+        cur_jump = JUMP_SPEED;
+    }
+    playerFall();
 
     drawBackground();
     drawPlayer();
@@ -39,6 +46,7 @@ $(document).ready(function(){
 
     W_WIDTH = document.documentElement.clientWidth;
     W_HEIGHT = document.documentElement.clientHeight;
+    GROUND_LEVEL = W_HEIGHT - GROUND_LEVEL;
 
     var canvas = document.getElementById('canvas');
     canvas.width = W_WIDTH;
@@ -48,7 +56,8 @@ $(document).ready(function(){
 
 
     player_x = Math.round(W_WIDTH / 2);
-    player_y = Math.round(W_HEIGHT / 5 * 4);
+    player_y = GROUND_LEVEL - P_HEIGHT;
+    cur_jump = 0;
 
     main();
 });
