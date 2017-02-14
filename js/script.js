3cb695a04e5967da86890abadd4de02698007085
AlexFSmirnov <alex.f.smirnov@yandex.ru>
2017-02-14 22:41:58 +0300

    Tried adding sprites

--- js/script.js
+++ js/script.js
@@ -7,8 +7,24 @@ function drawBackground() {
 }
 
 function drawPlayer() {
-    ctx.fillStyle = "rgb(0, 0, 0)";
-    ctx.fillRect(player_x, player_y, P_WIDTH, P_HEIGHT);
+    if (cur_action == 'still') {
+        if (Date.now() - prev_sprite_change > SCP_STILL) {
+            cur_cat_still = (cur_cat_still + 1) % 2;
+            prev_sprite_change = Date.now();   
+        }
+        cur_image = cats_still[cur_cat_still];
+    } else if (cur_action == 'walk') {
+        console.log(cur_cat_walk);
+        if (Date.now() - prev_sprite_change > SCP_WALK) {
+            cur_cat_walk = (cur_cat_walk + 1) % 4;
+            prev_sprite_change = Date.now();   
+        }
+        cur_image = cats_walk[cur_cat_walk];
+    } else if (cur_action == 'jump') {
+        cur_image = cat_jump;
+    }
+
+    ctx.drawImage(cur_image, player_x, player_y, P_WIDTH, P_HEIGHT);
 }
 
 function playerFall() {
@@ -17,10 +33,20 @@ function playerFall() {
 }
 
 function main() {
-    if (isPressed(left_keys))
+    if (player_y != GROUND_LEVEL - P_HEIGHT) {
+        cur_action = 'jump';
+    } else if (isPressed(left_keys) || isPressed(right_keys)) {
+        cur_action = 'walk';
+    } else {
+        cur_action = 'still';
+    }
+
+    if (isPressed(left_keys)) {
         player_x = Math.max(player_x - P_SPEED, 0);
-    if (isPressed(right_keys))
+    } 
+    if (isPressed(right_keys)) {
         player_x = Math.min(player_x + P_SPEED, W_WIDTH - P_WIDTH);
+    } 
     if (isPressed(up_keys) && player_y == GROUND_LEVEL - P_HEIGHT) {
         cur_jump = JUMP_SPEED;
     }
@@ -44,9 +70,8 @@ $(document).ready(function(){
     window.onkeydown = function(e) {keys_pressed[e.keyCode]=true;}
     window.onkeyup = function(e) {keys_pressed[e.keyCode]=false;}
 
-    W_WIDTH = document.documentElement.clientWidth;
-    W_HEIGHT = document.documentElement.clientHeight;
     GROUND_LEVEL = W_HEIGHT - GROUND_LEVEL;
+    JUMP_SPEED += GRAVITY;
 
     var canvas = document.getElementById('canvas');
     canvas.width = W_WIDTH;
@@ -58,6 +83,26 @@ $(document).ready(function(){
     player_x = Math.round(W_WIDTH / 2);
     player_y = GROUND_LEVEL - P_HEIGHT;
     cur_jump = 0;
+    cur_action = 'walk';
 
+    prepareSprites();
     main();
 });
+
+function prepareSprites() {
+    prev_sprite_change = Date.now();
+
+    cat_still_1 = new Image(); cat_still_1.src = "images/cat_still_1.gif";
+    cat_still_2 = new Image(); cat_still_2.src = "images/cat_still_2.gif";
+    cats_still = [cat_still_1, cat_still_2];
+    cur_cat_still = 1;
+
+    cat_walk_1 = new Image(); cat_walk_1.src = "images/cat_walk_1.gif";
+    cat_walk_2 = new Image(); cat_walk_2.src = "images/cat_walk_2.gif";
+    cat_walk_3 = new Image(); cat_walk_3.src = "images/cat_walk_3.gif";
+    cat_walk_4 = new Image(); cat_walk_4.src = "images/cat_walk_4.gif";
+    cats_walk = [cat_walk_1, cat_walk_2, cat_walk_3, cat_walk_4];
+    cur_cat_walk = 3;
+
+    cat_jump = new Image(); cat_jump.src = "images/cat_jump.gif";
+}
